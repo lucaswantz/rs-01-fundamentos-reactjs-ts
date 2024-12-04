66ad24cf3c16fdbd004f3cbe0a629d25d0e29cb6
Lucas W. da Motta <lucaswantzg@gmail.com>
2024-12-04 16:46:51 -0300

    Aprimorando a aplicação - Extensão de interfaces

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import { Header } from "./components/Header";
-import { Post } from "./components/Post";
+import { Post, PostType } from "./components/Post";
 import { Sidebar } from "./components/Sidebar";
 
 import styles from "./App.module.css";
-
 import "./global.css";
 
-const posts = [
+const posts: PostType[] = [
   {
     id: 1,
     author: {
@@ -53,12 +52,7 @@ function App() {
         <Sidebar />
         <main>
           {posts.map((post) => (
-            <Post
-              key={post.id}
-              author={post.author}
-              content={post.content}
-              publishedAt={post.publishedAt}
-            />
+            <Post key={post.id} post={post} />
           ))}
         </main>
       </div>
